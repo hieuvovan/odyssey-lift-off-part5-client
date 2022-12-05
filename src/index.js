5813ed431bbce523b550e6cc1c4b6d6a533b132e
Vo Van Hieu <vovanhieu@HieuVo.local>
2022-12-05 10:19:58 +0700

    config uri

--- src/index.js
+++ src/index.js
@@ -5,7 +5,10 @@ import Pages from "./pages";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "https://server-catstronauts.herokuapp.com/", // change to YOUR own production server
+  uri:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:4000"
+      : process.env.REACT_APP_URI, // change to YOUR own production server
   cache: new InMemoryCache(),
   name: "web",
   version: "1.0",
